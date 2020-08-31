541c0e22d96ceb4f0916048dd3862d72a56a1ea1
James Clarkson <jamesiclarkson@yahoo.co.uk>
2020-08-31 15:01:54 +0100

    dodgems charges less for kids. MVP complete

--- src/main/java/attractions/Dodgems.java
+++ src/main/java/attractions/Dodgems.java
@@ -1,8 +1,24 @@
 package attractions;
 
-public class Dodgems extends Attraction {
+import behaviours.ITicketed;
+import people.Visitor;
+
+public class Dodgems extends Attraction implements ITicketed {
 
     public Dodgems(String name, int rating) {
         super(name, rating);
+    }
+
+    public double defaultPrice() {
+        return 4.50;
+    }
+
+    public double priceFor(Visitor visitor) {
+        int visitorAge = visitor.getAge();
+        if(visitorAge < 12) {
+            return (defaultPrice() / 2.0);
+        } else {
+            return defaultPrice();
+        }
     }
 }
--- src/test/java/attractions/DodgemTest.java
+++ src/test/java/attractions/DodgemTest.java
@@ -2,16 +2,19 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class DodgemTest {
 
     Dodgems dodgems;
+    Visitor visitor;
 
     @Before
     public void setUp() throws Exception {
         dodgems = new Dodgems("Bumper Cars", 5);
+        visitor = new Visitor(11, 1.1, 50.99);
     }
 
 
@@ -28,5 +31,15 @@ public class DodgemTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, dodgems.getVisitCount());
+    }
+
+    @Test
+    public void hasDefaultPrice() {
+        assertEquals(4.50, dodgems.defaultPrice(), 0.01);
+    }
+
+    @Test
+    public void halfPriceForKids() {
+        assertEquals(2.25, dodgems.priceFor(visitor), 0.01);
     }
 }
